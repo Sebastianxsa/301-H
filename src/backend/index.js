2a8a0b274cca15606c8f1331838b47a391d21608
Hardy <1298829@senati.pe>
2025-10-28 12:06:16 -0500

    fix:Correccion invocacion de JWT_SECRET

--- src/backend/index.js
+++ src/backend/index.js
@@ -1,10 +1,11 @@
-import express from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import userRoutes from './routes/userRoutes.js';
-import emailRoutes from './routes/emailRoutes.js';
-import swaggerUi from 'swagger-ui-express';
-import { swaggerSpec } from './swagger.js';
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import userRoutes from "./routes/userRoutes.js";
+import emailRoutes from "./routes/emailRoutes.js";
+import authRoutes from "./routes/authRoutes.js";
+import swaggerUi from "swagger-ui-express";
+import { swaggerSpec } from "./swagger.js";
 
 dotenv.config();
 
@@ -14,19 +15,20 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
-app.use('/api-docs', swaggerUi.serve,swaggerUi.setup(swaggerSpec));
-app.use(express.urlencoded({extended:true}));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/',(req, res) => {
-    res.json({
-        message:'Api corriendo correctamente'
-    });
+app.get("/", (req, res) => {
+  res.json({
+    message: "Api corriendo correctamente",
+  });
 });
 
 //Rutas que deseo usar
-app.use('/api/users',userRoutes);
-app.use('/api/email',emailRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/email", emailRoutes);
+app.use("/api/auth", authRoutes);
 
-app.listen(PORT,()=>{
-    console.log("Servidor corriendo exitosamente 🚀")
-})
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo y escuchando en el puerto ${PORT}`);
+});
